eb4672b0173befad6b8ea45f563404e9788dabe3
Febiyan Rachman <febiyan.rachman@thinkbiganalytics.com>
2018-01-30 11:54:32 +0100

    test

--- api-auth-express/app-config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-module.exports = {  
-    jwtIssuer: "Febiyan's Web Service",
-    jwtSecret: "MyS3cr3tK3Y",
-    jwtSession: {
-        session: false
-    },
-    jwtDuration: 1440
-};
