cc20ab14d62dda61cc49b614f120dad7bab3d79b
Febiyan Rachman <febiyan.rachman@thinkbiganalytics.com>
2018-02-09 13:48:33 +0100

    Finalise the spark streaming to Kafka
    Will need to revamp the Express JS and UI now to reflect changes in Kafka

--- stream-processing-spark/readstream.scala
+++ stream-processing-spark/readstream.scala
@@ -1,10 +1,6 @@
 import spark.implicits._
+import org.apache.spark.sql._
 import org.apache.spark.sql.types._
-
-val schema = StructType(Array(
-    StructField("category", StringType),
-    StructField("sales", DoubleType)
-))
 
 // Set up the Kafka connection
 val kafkaDF = spark.
@@ -17,24 +13,87 @@ val kafkaDF = spark.
 
 // Parse the received JSON data from Kafka
 var streamingDF = kafkaDF.
-    select(($"value").cast("string").alias("data")).
+    select(($"value").cast(StringType).alias("data")).
     select(
         // Category column
-        get_json_object($"data", "$.category").cast("string").alias("category"),
+        get_json_object($"data", "$.category").cast(StringType).alias("category"),
         // Sales (money) column
-        get_json_object($"data", "$.sales").cast("double").alias("sales"),
+        get_json_object($"data", "$.sales").cast(DoubleType).alias("sales"),
         // Need to divide by 1000 to convert to seconds (Javascript Date.now() outputs in milliseconds)
-        from_unixtime(get_json_object($"data", "$.ts") / 1000).cast("timestamp").alias("ts")
+        from_unixtime(get_json_object($"data", "$.ts") / 1000).cast(TimestampType).alias("ts")
     );
 
-// Do a window calculation every 5 seconds
-// Tolerate late data up to 10 seconds
-var streamingWindowDF = streamingDF.
-    withWatermark("ts", "10 seconds").
-    groupBy($"category", window($"ts", "5 seconds")).
-    count();
+// Do a window of 4 seconds calculation every 2 seconds
+// Tolerate late data up to 4 seconds
+// The as[] is to convert dataframe to DS
+var streamingWindowDS = streamingDF.
+    withWatermark("ts", "4 seconds").
+    groupBy(
+        $"category",
+        // group by 4 seconds window, calculate every 2 seconds
+        window($"ts", "4 seconds")
+    ).
+    agg(
+        expr("sum(sales) as running_sum_sales")
+    )
 
-// Print to console
-val execution = streamingWindowDF.writeStream.format("console").start().awaitTermination();
+/*
+// For debugging purposes from spark-shell only
+// Stop by issuing 
+var queryConsole = streamingWindowDS.
+    writeStream.
+    format("console").
+    option("truncate", false).
+    start
+*/
 
-// df2.select(from_json($"value", schema).as("data")).writeStream.format("console").start().awaitTermination()
+// Spark 2.2.0 has kafka sink. Spark < 2.2.0 needs to implement ForeachWriter
+var query = streamingWindowDS.
+    writeStream.
+    format("kafka").
+    option("kafka.bootstrap.servers", "localhost:9092").
+    option("topic", "aggregations").
+    start
+
+
+/*
+// I don't know why the classes definitions here need a complete package.class notation
+// Pre spark 2.2.0 code below
+case class Output(category: String, running_sum_sales: Double)
+
+class  KafkaSink(topic:String, servers:String) extends org.apache.spark.sql.ForeachWriter[Output] {
+    val kafkaProperties = new  java.util.Properties()
+    kafkaProperties.put("bootstrap.servers", servers)
+    kafkaProperties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    kafkaProperties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+
+    val results = new scala.collection.mutable.HashMap[String, String]
+    var producer: org.apache.kafka.clients.producer.KafkaProducer[String, String] = _
+    var gson: com.google.gson.Gson = _
+
+    def open(partitionId: Long,version: Long): Boolean = {
+        producer = new org.apache.kafka.clients.producer.KafkaProducer(kafkaProperties)
+        gson = new com.google.gson.Gson
+        true
+    }
+
+    def process(value: Output): Unit = {
+        producer.send(new org.apache.kafka.clients.producer.ProducerRecord(topic, gson.toJson(value)))
+    }
+
+    def close(errorOrNull: Throwable): Unit = {
+        producer.close()
+    }
+}
+
+val topic = "aggregations"
+val brokers = "localhost:9092"
+val writer = new KafkaSink(topic, brokers)
+
+val query = streamingWindowDS.
+    writeStream.
+    foreach(writer).
+    outputMode("complete").
+    trigger(ProcessingTime("10 seconds"))
+    start()
+*/
